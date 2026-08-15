a94f436737d6180d9adea6926db804a0cc11f6be
M A D A N <madan20131312@gmail.com>
2026-08-15 16:23:51 +0530

    Create AudienceDAO.java

---
+++ AudienceDAO.java
@@ -0,0 +1,31 @@
+package com.journal.AudienceDAO;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.journal.AdminPOJO.AdminPOJO;
+import com.journal.AudiencePOJO.AudiencePOJO;
+
+@Repository
+public class AudienceDAO {
+	private SqlMapClient smc;
+
+	public List<String> gettherespectivemovies(String ccom) throws IOException, SQLException {
+		Reader rd = Resources.getResourceAsReader("sqlMapClient.xml");
+		smc = SqlMapClientBuilder.buildSqlMapClient(rd);
+		return smc.queryForList("Audience.getallmovies", ccom);
+	}
+
+	public int insertUser(AudiencePOJO audience) throws SQLException, IOException {
+		Reader rd = Resources.getResourceAsReader("sqlMapClient.xml");
+		smc = SqlMapClientBuilder.buildSqlMapClient(rd);
+		smc.insert("Audience.insertaudience", audience);
+		return 1;
+	}
+}
